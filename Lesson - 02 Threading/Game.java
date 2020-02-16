0bab63df76072587ff0b87b4f7961e0d99efbda9
vasilijus <vasiliokas@gmail.com>
2020-02-16 12:25:46 +0000

    Forgot to pass the game , to the Thread( % )

--- Lesson - 02 Threading/Game.java
+++ Lesson - 02 Threading/Game.java
@@ -63,7 +63,7 @@ public class Game extends JFrame implements Runnable
     public static void main(String[] args)
     {
         Game game = new Game();
-        Thread gameThread = new Thread();
+        Thread gameThread = new Thread(game);
         gameThread.start();
     }
 }
