a98fcfe758884c19bb4b316e2a018068c04e0895
vasilijus <vasiliokas@gmail.com>
2020-02-17 00:16:55 +0000

    render Rectangles

--- Lesson - 05 Perspective/Definitions.md
+++ Lesson - 05 Perspective/Definitions.md
@@ -1,3 +1,9 @@
 # Perspective
 
 # Rectangle
+
+ - A new class made only to hold 4 fields
+ - X Y W H - X position , Y position, Width, Height.
+ - We can use rectangles to represent many things. Like the Camera ( the user viewport), game objects like the player , and even tiles.
+ - For debug purposes or possibly other purposes, we may want to render a rectangle on to the screen.
+ - Create a renderRectangle() method in RenderHandler.
--- Lesson - 05 Perspective/RenderHandler.java
+++ Lesson - 05 Perspective/RenderHandler.java
@@ -7,6 +7,7 @@ public class RenderHandler
 {
 
     private BufferedImage view;
+    private Rectangle camera;
     private int[] pixels;
 
     // Constructor
@@ -14,6 +15,12 @@ public class RenderHandler
     {
         // Create a BufferedImage that will represent the view
         view = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        //
+        camera = new Rectangle(0, 0, width, height);
+
+        camera.x = -100;
+        camera.y = -30;
 
         // Create an array for pixels
         pixels = ((DataBufferInt) view.getRaster().getDataBuffer()).getData();
@@ -31,21 +38,32 @@ public class RenderHandler
     {
         int[] imagePixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
-        for(int y = 0; y < image.getHeight(); y++ ) 
-            for(int x = 0; x < image.getWidth(); x++ ) 
+        renderArray(imagePixels, image.getWidth(), image.getHeight(), xPosition, yPosition, xZoom, yZoom);
+    } 
+
+    public void renderRectangle( Rectangle rectangle, int xZoom, int yZoom)
+    {
+
+    }
+
+    public void renderArray(int[] renderPixels, int renderWidth, int renderHeight, int xPosition, int yPosition, int xZoom, int yZoom)
+    {
+        for(int y = 0; y < renderHeight; y++ ) 
+            for(int x = 0; x < renderWidth; x++ ) 
                 for(int xZoomPosition = 0; xZoomPosition < xZoom; xZoomPosition++)
                     for(int yZoomPosition = 0; yZoomPosition < yZoom; yZoomPosition++)
-                        setPixel(imagePixels[x + y * image.getWidth()], (x * xZoom)+xPosition+xZoomPosition, (y*yZoom)+yPosition+yZoomPosition );      
-
+                    setPixel(renderPixels[x + y * renderWidth], (x * xZoom)+xPosition+xZoomPosition, (y*yZoom)+yPosition+yZoomPosition );      
     }
 
     private void setPixel( int pixel, int x, int y)
     {
-        int pixelIndex = x + y * view.getWidth();
-        if(pixels.length > pixelIndex) {
-            pixels[pixelIndex] = pixel;
+        if (x >=camera.x && y >= camera.y && x <= camera.x + camera.w && y <= camera.y + camera.h) {
+            int pixelIndex = (x - camera.x) + (y - camera.y) * view.getWidth();
+            if(pixels.length > pixelIndex) {
+                pixels[pixelIndex] = pixel;
+            }
         }
-
     }
+
 
 }
