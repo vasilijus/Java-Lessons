0e829b925ed820c117d2baec57353c7c36630d54
vasilijus <vasiliokas@gmail.com>
2020-03-14 15:02:09 +0000

    Lesson 12 init

---
+++ Lesson - 12 GUI and palletes/AnimatedSprite.java
@@ -0,0 +1,87 @@
+import java.awt.image.BufferedImage;
+
+public class AnimatedSprite extends Sprite implements GameObject {
+
+    private Sprite[] sprites;
+    private int currentSprite = 0;
+    private int speed;
+    private int counter;
+
+    private int startSprite = 0;
+    private int endSprite;
+
+    public AnimatedSprite( SpriteSheet sheet, Rectangle[] positions, int speed ) 
+	{
+		sprites = new Sprite[ positions.length ];
+        this.speed = speed;
+        this.endSprite = positions.length - 1;
+    }
+    
+    
+    public AnimatedSprite( SpriteSheet sheet,  int speed ) 
+	{
+		sprites = sheet.getLoadedSprites();
+        this.speed = speed;
+        this.endSprite = sprites.length - 1;
+	}
+
+    // @ param speed represents how many frames pass until the sprite changes
+    public AnimatedSprite(BufferedImage[] images, int speed) {
+
+        sprites = new Sprite[images.length];
+        this.speed = speed;
+        this.startSprite = images.length - 1;
+
+        for (int i = 0; i < images.length; i++)
+            sprites[i] = new Sprite(images[i]);
+    }
+
+    // call this every time physically possible ( Render is dealth specifically with
+    // the Layer class )
+    public void render(RenderHandler renderer, int xZoom, int yZoom) {
+    }
+
+    // call at 60 fps rate
+    public void  update(Game game) 
+    {
+        counter++;
+        if (counter >= speed) 
+        {
+            counter = 0;
+            incrementSprite();
+        }
+    }
+
+    public void reset()
+    {
+        counter = 0;
+        currentSprite = startSprite;
+    }
+
+    public void setAnimationRange(int startSprite, int endSprite ) 
+    {
+        this.startSprite    = startSprite;
+        this.endSprite      = endSprite;
+        reset();
+    }
+
+    public int getWidth() {
+        return sprites[currentSprite].getWidth();
+    }
+
+    public int getHeight() {
+        return sprites[currentSprite].getHeight();
+    }
+
+    public int[] getPixels() {
+        return sprites[currentSprite].getPixels();
+    }
+
+    public void incrementSprite() {
+        currentSprite++;
+
+        if (currentSprite >= endSprite)
+            currentSprite = startSprite;
+    }
+
+}
---
+++ Lesson - 12 GUI and palletes/Player.java
@@ -0,0 +1,102 @@
+public  class Player implements GameObject // cannot be abstract ??? abstract
+{
+    private Rectangle playerRectangle;
+    private int speed = 10;
+    private Sprite sprite;
+    private AnimatedSprite animatedSprite = null;
+    // 0 = right, 1 = left , 2 = up , 3 = down
+    private int direction = 0;
+
+    public Player( Sprite sprite ) {
+        this.sprite = sprite;
+        if( sprite instanceof AnimatedSprite )
+            animatedSprite = ( AnimatedSprite) sprite;
+
+        updateDirection();
+        playerRectangle = new Rectangle( 20, 16, 16, 20 );
+        playerRectangle.generateGraphics(2 , 0x00FF0090 );
+
+    }
+
+    private void updateDirection()
+    {
+        if( animatedSprite != null)
+        {
+            animatedSprite.setAnimationRange( direction * 8, ( direction * 8) + 7 );
+        }
+    }
+
+    // Call every time physically possible
+    public void render( RenderHandler renderer , int xZoom, int yZoom ) {
+        //renderer.renderRectangle( playerRectangle, xZoom, yZoom );
+        if(animatedSprite != null) {
+            // System.out.println("Render: animatedSprite != null");
+            renderer.renderSprite(animatedSprite, playerRectangle.x, playerRectangle.y, xZoom, yZoom );
+        } else if (sprite != null ) {
+            System.out.println("Render: sprite != null");
+            renderer.renderSprite(sprite, playerRectangle.x, playerRectangle.y, xZoom, yZoom );
+        } else {
+            System.out.println("Render: Else");
+            renderer.renderRectangle(playerRectangle, xZoom, yZoom  );
+        }
+
+    }
+
+    // Call at 60 fps rate
+    public void update( Game game ) {
+
+        KeyBoardListener keyListener = game.getKeyListener();
+
+        boolean didMove = false;
+        int newDirection = direction;
+
+        if ( keyListener.left() ) {
+            newDirection = 1;
+            didMove = true;
+            playerRectangle.x -= speed;
+            
+        }
+          
+        if ( keyListener.right() ) {
+            newDirection = 0;
+            didMove = true;
+            playerRectangle.x += speed;
+            
+        }
+
+        if ( keyListener.up() ){
+            newDirection = 2;
+            didMove = true;
+            playerRectangle.y -= speed;
+            
+        }
+            
+        if ( keyListener.down() ) {
+            newDirection = 3;
+            didMove = true;
+            playerRectangle.y += speed;
+            
+        }
+      
+        if ( newDirection != direction ){
+            direction = newDirection;
+            updateDirection();
+        }
+        
+        if ( !didMove ) {
+            animatedSprite.reset();
+        }
+
+        updateCamera( game.getRenderer().getCamera() );
+
+        if (didMove)
+            animatedSprite.update(game);
+
+    }
+
+    public void updateCamera( Rectangle camera ) {
+        camera.x = playerRectangle.x - ( camera.w / 2 );
+        camera.y = playerRectangle.y - ( camera.h / 2 );
+    }
+
+}
